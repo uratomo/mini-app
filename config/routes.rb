80c3b5ad746c13d740ea5662aaec693ee68fbd68
uratomo <rop.ibuki@gmail.com>
2019-06-12 15:20:36 +0900

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
   resources :tweets
-  resources :users,only: [:show]
 end
