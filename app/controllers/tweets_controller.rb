013f6f31acd36f7099bc3c6b2657f42d849cd3b7
uratomo <rop.ibuki@gmail.com>
2019-06-12 10:23:03 +0900

    tweet_controller make

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,14 +3,25 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet= Tweet.new
   end
 
   def create
+    @tweets = Tweet.create(tweet_params)
+    redirect_to action: :index
   end
   
-  def show
+  def edit
+    @tweet=Tweet.find(params[:id])
   end
 
   def update
+
+  end
+
+  private
+
+  def tweet_params
+  params.require(:tweet).permit(:text,).merge(user_id:current_user.id)
   end
 end
